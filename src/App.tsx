8a731c41790a772b344f4cc040b1406030390706
Joseph Ditton <jditton.atomic@gmail.com>
2023-01-23 13:04:18 -0700

    adds app

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import { recordAnalytic } from './lib/recordAnalytic';
+let ID_COUNT = 0;
+
+interface Todo {
+  id: number;
+  description: string;
+  completed: boolean;
+}
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [description, setDescription] = useState("");
+  const [analyticRecorded, setAnalyticRecorded] = useState(false);
+
+  useEffect(() => {
+    if (todos.length === 2 && !analyticRecorded) {
+      recordAnalytic();
+      setAnalyticRecorded(true);
+    }
+  }, [todos, analyticRecorded]);
+
+  async function loadRandomQuote() {
+    const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
+    console.log(await result.json());
+  }
+
+  function saveTodo() {
+    if (description === "") return;
+    const todo: Todo = {
+      id: ID_COUNT++,
+      description,
+      completed: false
+    };
+    setTodos([...todos, todo]);
+  }
+
+  function toggleTodo(todo: Todo) {
+    todo.completed = !todo.completed;
+    setTodos([...todos]);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={description}
+          placeholder="The thing you need to do"
+          onChange={e => setDescription(e.target.value)}
+        />
+        <button onClick={saveTodo}>Save</button>
+      </div>
+      <div>
+        {
+          todos.map((todo) => (
+            <div key={todo.id}>
+              <input checked={todo.completed} type="checkbox" onChange={() => toggleTodo(todo)}/> {todo.description}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
